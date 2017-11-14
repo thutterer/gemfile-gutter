7dc089d28a7f1541545a91d63108b0d4b285af5e
Thomas Hutterer <thutterer@suse.de>
2017-11-14 10:32:05 +0100

    replace demo view with dummy text manipulation

--- spec/gemfile-unlocked-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import GemfileUnlockedView from '../lib/gemfile-unlocked-view';
-
-describe('GemfileUnlockedView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
